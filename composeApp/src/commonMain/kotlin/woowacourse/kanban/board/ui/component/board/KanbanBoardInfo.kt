96533f9d78efbd8f5f24b5b46902cc5512725ac3
cksckckcks <chan679999@naver.com>
2026-03-24 16:37:05 +0900

    refactor: 파일 UI 패키지 생성 및 이동

---
+++ composeApp/src/commonMain/kotlin/woowacourse/kanban/board/ui/component/board/KanbanBoardInfo.kt
@@ -0,0 +1,10 @@
+package woowacourse.kanban.board.ui.component.board
+
+import androidx.compose.ui.graphics.Color
+
+class KanbanBoardInfo(
+    val title: String,
+    val titleBackgroundColor: Color,
+    val bodyColor: Color,
+    val borderColor: Color,
+)
