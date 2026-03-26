9056d99c6ce86a9fb60d25179b98478f649a51ee
cksckckcks <chan679999@naver.com>
2026-03-26 11:27:11 +0900

    refactor: KanbanProject 파라미터에 taskIds 추가

--- composeApp/src/commonMain/kotlin/woowacourse/kanban/board/domain/KanbanProject.kt
+++ composeApp/src/commonMain/kotlin/woowacourse/kanban/board/domain/KanbanProject.kt
@@ -1,8 +1,9 @@
 package woowacourse.kanban.board.domain
 
-class KanbanProject(val title: String) {
-    private val taskIds = mutableListOf<Long>()
-
+class KanbanProject(
+    val title: String,
+    private val taskIds: MutableList<Long> = mutableListOf<Long>(),
+) {
     fun getTaskIds(): List<Long> = taskIds
 
     fun addTaskId(taskId: Long) {
