7f7605c118b6d628fec9b3d8aa2a7a69e904c3d0
cksckckcks <chan679999@naver.com>
2026-04-02 17:30:08 +0900

    refactor: KanbanBoardScreenTest 테스트 코드 추가

--- composeApp/src/commonTest/kotlin/woowacourse/kanban/board/ui/screen/KanbanBoardScreenTest.kt
+++ composeApp/src/commonTest/kotlin/woowacourse/kanban/board/ui/screen/KanbanBoardScreenTest.kt
@@ -32,6 +32,7 @@ class KanbanBoardScreenTest {
 
         onNodeWithText("To Do").assertIsDisplayed()
         onNodeWithText("In Progress").assertIsDisplayed()
+        onNodeWithText("Review").assertIsDisplayed()
         onNodeWithText("Done").assertIsDisplayed()
     }
 
@@ -214,5 +215,27 @@ class KanbanBoardScreenTest {
         onNodeWithText("우아한테크코스").assertDoesNotExist()
         onNodeWithText("안드로이드").assertIsDisplayed()
         onNodeWithText("8기").assertIsDisplayed()
+    }
+
+    @Test
+    fun `태스크를 생성 후 클릭했을 때 수정다이얼로그가 정상적으로 표시된다`() = runComposeUiTest {
+        // Given
+        setContent {
+            CompositionLocalProvider(LocalDensity provides Density(0.1f)) {
+                KanbanBoardScreen(
+                    projects = listOf(KanbanProject("안녕")),
+                )
+            }
+        }
+
+        // When
+        onNodeWithText("새 태스크 생성").performClick()
+        onNodeWithText("태스크 제목을 입력하세요.").performTextInput("안녕하세요")
+        onNodeWithText("생성").performClick()
+        onNodeWithText("5자 이내의 태그를 최대 5개까지 등록할 수 있습니다.").assertDoesNotExist()
+        onNodeWithText("안녕하세요").performClick()
+
+        // Then
+        onNodeWithText("수정").assertIsDisplayed()
     }
 }
