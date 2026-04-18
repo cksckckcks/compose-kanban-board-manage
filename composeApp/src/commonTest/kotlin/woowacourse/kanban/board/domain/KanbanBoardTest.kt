337fa349666fa1ca0212320342b4a47f855715bc
cksckckcks <chan679999@naver.com>
2026-04-18 20:51:02 +0900

    test: KanbanBoard 테스트 코드 추가

--- composeApp/src/commonTest/kotlin/woowacourse/kanban/board/domain/KanbanBoardTest.kt
+++ composeApp/src/commonTest/kotlin/woowacourse/kanban/board/domain/KanbanBoardTest.kt
@@ -1,9 +1,9 @@
 package woowacourse.kanban.board.domain
 
-import kotlin.test.Test
 import org.assertj.core.api.Assertions.assertThat
 import woowacourse.kanban.board.domain.dialog.Status
 import woowacourse.kanban.board.domain.result.BoardResult
+import kotlin.test.Test
 
 class KanbanBoardTest {
     @Test
@@ -12,7 +12,7 @@ class KanbanBoardTest {
         val projects = listOf(
             KanbanProject(title = "안녕"),
 
-        )
+            )
         var kanbanBoard = KanbanBoard(projects = projects)
 
         // When
@@ -52,7 +52,7 @@ class KanbanBoardTest {
                 ),
             ),
 
-        )
+            )
 
         var kanbanBoard = KanbanBoard(projects = projects)
 
@@ -63,5 +63,57 @@ class KanbanBoardTest {
         // Then
         assertThat(kanbanBoard.getProject(0).getTasksByStatus(Status.TO_DO).size).isEqualTo(0)
         assertThat(kanbanBoard.getProject(0).getTasksByStatus(Status.IN_PROGRESS).size).isEqualTo(1)
+    }
+
+    @Test
+    fun `칸반 보드의 project들을 반환한다`() {
+        val task = KanbanTask(
+            id = 4L,
+            title = "새로운 기능 구현",
+            description = "이 기능은 매우 중요합니다.",
+            tags = listOf("긴급", "백엔드"),
+            status = Status.TO_DO,
+            assignee = "별터",
+        )
+
+        val projects = listOf(
+            KanbanProject(
+                title = "안녕",
+                tasks = listOf(
+                    task,
+                ),
+            ),
+        )
+
+        val kanbanBoard = KanbanBoard(projects = projects)
+        val boardProjects = kanbanBoard.getProjectList()
+
+        assertThat(boardProjects).isEqualTo(projects)
+    }
+
+    @Test
+    fun `칸반 보드의 project의 제목들을 반환한다`() {
+        val task = KanbanTask(
+            id = 4L,
+            title = "새로운 기능 구현",
+            description = "이 기능은 매우 중요합니다.",
+            tags = listOf("긴급", "백엔드"),
+            status = Status.TO_DO,
+            assignee = "별터",
+        )
+
+        val projects = listOf(
+            KanbanProject(
+                title = "안녕",
+                tasks = listOf(
+                    task,
+                ),
+            ),
+        )
+
+        val kanbanBoard = KanbanBoard(projects = projects)
+        val boardProjects = kanbanBoard.getProjectTitles()
+
+        assertThat(boardProjects).isEqualTo(listOf("안녕"))
     }
 }
